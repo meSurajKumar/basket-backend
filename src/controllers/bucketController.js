2559abaf99ca3ce12de292ed95baed744655b589
prof-parad0x <professorparadox19@gmail.com>
2024-04-26 00:15:01 +0530

    ball saved

--- src/controllers/bucketController.js
+++ src/controllers/bucketController.js
@@ -34,43 +34,52 @@ class BasketController{
     */
     addBalls = async (req, res) => {
         try {
-            let ballsVolumeData = []
-            let totalVolume=0;
+            let transformedBallData = [];
             const ballsData = req.body.ballData;
-            
-            for(let i=0;i<ballsData.length;i++){
-                let ballName = ballsData[i].ballName
-                let ballcount = ballsData[i].ballCounts
-                const balls = await Ball.findOne({ballColor:ballName}).select('-_id -__v');
-                ballsVolumeData.push({'ballColor' : balls.ballColor , volumeOccupy :balls.ballVolume* ballcount})
-                console.log('ballsVolumeData :: ',ballsVolumeData)              
-                totalVolume = totalVolume + balls.ballVolume* ballcount
-            }
-            
-            // Sort buckets by available volume in ascending order
-            const buckets = await Bucket.find().sort({ bucketVolumeLeft: 1 });
-
-            let ballsPlaced = 0;
-            for (const bucket of buckets) {
-                if (totalVolume === 0) break; // All balls placed, exit loop
-                const spaceLeft = bucket.bucketVolumeLeft;
-                console.log('sapce let ::: ' , spaceLeft)
-                if (spaceLeft > 0) {
-                    const volumeToAdd = Math.min(spaceLeft, totalVolume);
-                    // Update BucketData with new ball data
-                    bucket.ballsData.push(...ballsVolumeData.map(ball => ({ ballColor: ball.ballColor, ballCounts: ball.volumeOccupy })));
-                    bucket.bucketVolumeLeft -= volumeToAdd;
-                    await bucket.save();
-                    totalVolume -= volumeToAdd;
-                    ballsPlaced += volumeToAdd;
+    
+            // Transform ballData array to ensure each ball count is represented as a separate object
+            for (const ball of ballsData) {
+                for (let i = 0; i < ball.ballCounts; i++) {
+                    transformedBallData.push({
+                        ballName: ball.ballName,
+                        ballCounts: 1
+                    });
                 }
             }
-
-            if (ballsPlaced === 0) {
-                return res.status(400).send({ message: "No balls were placed in any bucket." });
-            }
-            return res.status(200).send({ message: `${ballsPlaced} volume of balls were placed in buckets successfully.` });   
     
+            // Now transformedBallData contains individual balls with count 1 each
+            console.log("Transformed Ball Data:", transformedBallData);
+    
+            // Iterate through each ball and place it in a bucket
+            for (const ball of transformedBallData) {
+                let ballStored = false;
+    
+                // Fetch the volume of the ball from the database
+                const ballData = await Ball.findOne({ ballColor: ball.ballName });
+                const ballVolume = ballData.ballVolume;
+    
+                // Find a bucket with enough space to store the ball
+                const bucket = await Bucket.findOneAndUpdate(
+                    { bucketVolumeLeft: { $gte: ballVolume } },
+                    { $push: { ballsData: { ballName: ball.ballName, ballCounts: 1 } }, $inc: { bucketVolumeLeft: -ballVolume } },
+                    { sort: { bucketVolumeLeft: 1 }, new: true }
+                );
+    
+                if (bucket) {
+                    ballStored = true;
+                    console.log(`Ball '${ball.ballName}' stored in bucket '${bucket.bucketName}'.`);
+                } else {
+                    console.log(`No bucket available to store ball '${ball.ballName}'.`);
+                }
+    
+                if (!ballStored) {
+                    // Handle the case where the ball couldn't be stored in any bucket
+                    return res.status(400).send({ message: `Ball '${ball.ballName}' couldn't be stored.` });
+                }
+            }
+    
+            // All balls stored successfully
+            return res.status(200).send({ message: "All balls stored successfully in buckets." });
         } catch (error) {
             console.error("Error in addBalls method:", error);
             return res.status(500).send({ message: "An error occurred while placing balls in buckets." });
