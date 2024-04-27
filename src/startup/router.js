e263fc20d2facd07a6af6e3dacd4cfa91d889c17
prof-parad0x <professorparadox19@gmail.com>
2024-04-27 13:25:26 +0530

    added swagger doc

--- src/startup/router.js
+++ src/startup/router.js
@@ -1,6 +1,11 @@
 import bucketRoutes from '../routes/bucketRoutes.js';
 import ballRoutes from '../routes/ballRoutes.js'
+import fs from 'fs'
 
+// importing the swagger doc and swagger ui
+import swaggerUI from 'swagger-ui-express'; 
+const loadJSON = (path) => JSON.parse(fs.readFileSync(new URL(path, import.meta.url)));
+const swaggerDoc = loadJSON('../swagger/Basket-swagger.json');
 
 export default function (app) {
     app.get('/', (req, res) => {
@@ -8,6 +13,8 @@ export default function (app) {
     }) // this is just a test route of our app.
     app.use('/api/v1/bucket', bucketRoutes)
     app.use('/api/v1/ball', ballRoutes)
+
+    app.use('/api-doc',swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 
     app.use('*', (req, res) => {
         return res.status(404).send({ message: 'The route you are looking for not exists !' }) // if the route not exists.
